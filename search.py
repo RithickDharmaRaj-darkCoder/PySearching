a74cdc286ec92678b879eee70ebe755632b2880e
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-05-23 05:14:52 +0530

    collection mode of searching...

--- search.py
+++ search.py
@@ -1,12 +1,14 @@
 # calling of list...
 from srch_lst import *
 
-aval_srch = ['Linear']
+aval_srch = ['Linear','Binary']
 print("Available Searching Methods : ",aval_srch)
 flst = input("Enter the search name : ").upper()
 
 if flst == 'LINEAR':
     linear()
+elif flst == 'BINARY':
+    binary()
 else:
     print("Invalid Answer!")
 
--- srch_lst.py
+++ srch_lst.py
@@ -11,9 +11,31 @@ def linear():
 
     for i in range(len(lst)):
         if lst[i] == fnum:
-            print(f"Element fount at [ {i+1}th ] position in list.")
+            print(f"Element fount at position {i+1}.")
             break
     else:
         print("Element not in the list!")
 
+def binary():
+    lst = []
+    lsize = int(input("\nEnter how many numbers to be insert : "))
+    for i in range(lsize):
+        elst = int(input("Enter the numbers to add it in list : "))
+        lst.append(elst)
+    lst.sort()
+    print(lst)
+    fnum = int(input("\nEnter the numbers to find : "))
 
+    l = 0
+    u = len(lst) - 1
+    while l <= u:
+        mid = (l + u) // 2
+        if lst[mid] == fnum:
+            print(f"{fnum} is found at position {mid + 1}.")
+            break
+        elif lst[mid] < fnum:
+            l = mid + 1
+        elif lst[mid] > fnum:
+            u = mid - 1
+    else:
+        print("Number not in the list!")

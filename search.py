5081a4e7bfaee7b31c223ad50e444469f6ff777c
RithickDharmaRaj <rithick.darkcoder@gmail.com>
2021-06-17 02:37:18 +0530

    Alphabets in Horizontal way...l

--- search.py
+++ search.py
@@ -1,17 +1,24 @@
 # calling of list...
 from srch_lst import *
+aval_srch = ['Linear Search','Binary Search']
+print("Available Searching...")
+count = 1
+for i in aval_srch:
+    print(f'    {count}. {i},')
+    count += 1
 
-aval_srch = ['Linear','Binary']
-print("Available Searching Methods : ",aval_srch)
-flst = input("Enter the search name : ").upper()
+def searching():
+    search_iput = input("Enter the search name : ").upper()
 
-if flst == 'LINEAR':
-    linear()
-elif flst == 'BINARY':
-    binary()
-else:
-    print("Invalid Answer!")
+    if search_iput == 'LINEAR' or search_iput == 'LINEAR SEARCH' or search_iput == '1':
+        linear()
+    elif search_iput == 'BINARY' or search_iput == 'BINARY SEARCH' or search_iput == '2':
+        binary()
+    else:
+        print("Invalid Answer!")
+        searching()
 
+searching()
 
 #Personal Greetings...
 print("\nThank You!\n          -darkCoder \U0001F43E")
